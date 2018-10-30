6f50a3978f1d5a08e276eec7ca92c116af00056a
nirodhie <lteska2@gmail.com>
2018-10-30 13:54:28 +0100

    dodana konwersja na gigabajty i zaokrąglanie w górę

--- queriesMain.py
+++ queriesMain.py
@@ -1,4 +1,5 @@
 import math
+import subprocess
 
 import wmi
 
@@ -32,3 +33,17 @@ wmi_zapytanie('NumberOfCores', 'Win32_Processor')
 wmi_zapytanie('NumberOfLogicalProcessors', 'Win32_Processor')
 wmi_zapytanie('name', 'Win32_Processor')
 wmi_zapytanie_GB('size', 'Win32_diskdrive')
+wmi_zapytanie('caption', 'Win32_diskdrive')
+wmi_zapytanie('serialnumber', 'Win32_diskdrive')
+
+
+def bitlocker_key(linenumber):
+    process1 = subprocess.Popen(['cmd.exe', '/c', 'manage-bde', '-protectors', 'C:', '-get'], stdout=subprocess.PIPE)
+    output_as_list = []
+    for line in process1.stdout:
+        output_as_list.append(str(line).replace(r"\r\n'", "").replace("b'", "").replace("None", ""))
+    # print('\n'.join(output_as_list))
+    return output_as_list[linenumber]
+
+
+print(bitlocker_key(9).lstrip())
