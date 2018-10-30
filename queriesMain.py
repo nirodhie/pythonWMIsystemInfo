b17d86e06232d6c7a9e637f53bbc6b66cbc9975f
nirodhie <lteska2@gmail.com>
2018-10-30 12:43:30 +0100

    dziaua robienie zapytan jako klasy

--- queriesMain.py
+++ queriesMain.py
@@ -1,7 +1,17 @@
 import wmi
 
-c = wmi.WMI()
-GB = 1073741824
+GB = 1073741824  # bytes 2 GB convert
 
-computerName = input(r"\\")
-print(computerName)
+# computerName = input(r"\\")
+
+computerName = 'dtpolbn7004'
+c = wmi.WMI(computerName)
+
+
+def wmi_zapytanie(wmi_value, wmi_class):
+    wql = ("SELECT " + wmi_value + " FROM " + wmi_class)
+    for result in c.query(wql):
+        print(getattr(result, wmi_value))
+
+
+wmi_zapytanie('serialnumber', 'win32_bios')
